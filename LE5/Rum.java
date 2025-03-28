b989fca41ebd666e4d55c6f18dd172aaab00af56
221B223 <221B223@juet.co.in>
2025-03-28 14:12:26 +0530

    LE5

---
+++ LE5/Rum.java
@@ -0,0 +1,8 @@
+class Rum extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add water");
+    }
+    protected void stir(){
+        System.out.println("stir for 30 sec");
+    }
+}
---
+++ LE5/Whiskey.java
@@ -0,0 +1,8 @@
+class Whiskey extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add Ice");
+    }
+    protected void stir(){
+        System.out.println("stir for 25 sec");
+    }
+}
